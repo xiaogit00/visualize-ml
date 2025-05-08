72f4e80bc1ae08efdb6e70a2349a5f8a8955646b
xiaogit00 <leibingguo@gmail.com>
2025-05-08 21:39:31 +0800

    some changes

--- src/visualize_ml/views.cljs
+++ src/visualize_ml/views.cljs
@@ -1,13 +1,5 @@
-(ns visualize-ml.views
-  (:require
-   [re-frame.core :as re-frame]
-   [visualize-ml.subs :as subs]
-   ))
+(ns visualize-ml.views)
 
 (defn main-panel []
-  (let [name (re-frame/subscribe [::subs/name])]
-    [:div
-     [:h1
-      "Logistic Regression"]
-     [:button {:on-click #(println "Hello")} "Click here"]
-     ]))
+  [:div
+   [:h2 "Hi"]])
