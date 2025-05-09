42530aabe45c2962caf8452c7b942c24c708c8ca
xiaogit00 <leibingguo@gmail.com>
2025-05-09 17:49:16 +0800

    some changes

--- src/visualize_ml/views.cljs
+++ src/visualize_ml/views.cljs
@@ -1,5 +1,25 @@
-(ns visualize-ml.views)
+(ns visualize-ml.views
+  (:require
+   [re-frame.core :as re-frame]
+   [visualize-ml.subs :as subs]))
+
+(defn text-input []
+  [:div.field
+   [:label.label "Name"]
+   [:div.control
+    [:input.input {:type "text" :placeholder "Text input"}]]])
+
+(defn select-input []
+  [:div.field
+   [:label.label "Subject"]
+   [:div.control
+    [:div.select
+     [:select
+      [:option "Select Dropdown"]
+      [:option "With options"]]]]])
 
 (defn main-panel []
-  [:div
-   [:h2 "Hi"]])
+  (let [name (re-frame/subscribe [::subs/name])]
+    [:div
+     [text-input]
+     [select-input]]))
