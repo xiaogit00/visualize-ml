e5b648fe4247d87c1b6bae0de69b3d3d48c83834
xiaogit00 <leibingguo@gmail.com>
2025-05-10 15:42:19 +0800

    some changes

--- src/visualize_ml/views.cljs
+++ src/visualize_ml/views.cljs
@@ -5,34 +5,56 @@
 
 (defn chart [] 
   [:div.column.is-two-fifths
-       [:h2.debug "charts"]])
+       [:h2 "charts"]])
+
 (defn x-matrix []
   [:div.column.is-one-fifths
    [:h2 "x"]])
+
 (defn y-matrix [] 
   [:div.column.is-one-fifths
    [:h2 "y"]])
+
 (defn params [] 
   [:div.column.is-one-fifths
    [:h2 "params"]])
+
 (defn z-matrix [] 
   [:div.column.is-one-fifths
    [:h2 "z"]])
 
+(defn bottom-control-panel [] 
+  [:div.is-fixed-bottom.columns
+   [:div.is-flex.is-flex-direction-column.column.is-two-fifths.is-justify-content-space-around.pt-0
+    [:div "Input Data"]
+    [:div.columns.mb-0
+     [:div.column.is-two-thirds.is-flex.is-align-items-center
+      [:p.pr-2 "x:"]
+      [:input.input {:type "text" :placeholder "1, 2, 3, 4..."}]
+      ]
+     [:div.column.is-one-third 
+      [:button.button "Initialize Params"]]]
+    
+    
+    [:div.is-flex.columns
+     [:div.column.is-two-thirds.is-flex.is-align-items-center
+      [:p.pr-2 "y:"]
+      [:input.input {:type "text" :placeholder "1, 2, 3, 4..."}]]]]]
+  )
+
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
-    [:<>
+    [:div.is-family-monospace 
      [:div.container
-      [:h2.has-text-centered "Logistic Regression"]
+      [:h1.has-text-centered "Logistic Regression"]
       ]
-     [:div.container.debug.columns
+     [:div.container.columns
       [chart]
       [x-matrix]
       [y-matrix]
       [params]
       [z-matrix]
-      
       ]
+    [bottom-control-panel]
      ]
-    
     ))
